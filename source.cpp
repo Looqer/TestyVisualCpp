f71f82ec728c88fa914082006864441c42a43212
looqer <lukaszsienkiewicz94@gmail.com>
2019-11-14 21:19:26 +0100

    eng 0.2

--- source.cpp
+++ source.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <ctime>
+
+int a, b;
+std::string signs;
+std::string tabledog[ ] = { "Hewi", "Sara", "Sonia" };
+std::string tabledog_name1[] = { "He", "Sa", "So" };
+std::string tabledog_name2[] = { "wi", "ra", "nia" };
 
 void my_function()
 {
@@ -6,11 +13,9 @@ void my_function()
 	std::cout << "[2] - substraction" << std::endl;
 	std::cout << "[3] - determ. the transfer function" << std::endl;
 	std::cout << "[4] - string test" << std::endl;
+	std::cout << "[5] - Where is a puppy?" << std::endl;
 	std::cout << "[0] - shutdown" << std::endl;
 }
-
-int a, b;
-std::string signs;
 
 void addition()
 {
@@ -42,8 +47,18 @@ void strings()
 	std::cout << "Twoje znaki to: " << signs << std::endl;
 }
 
+void puppies()
+{
+	std::cout << "Where is the dog?" << std::endl;
+	std::cout << "Give a number! (1-3)" << std::endl;
+	std::cin >> a;
+	std::cout << "Dog adress is a: " << & tabledog [a] << std::endl;
+	std::cout << "Now you have to get a new dog. It name's: " << tabledog_name1[(std::rand() % 3)] << tabledog_name2[(std::rand() % 3)] << std::endl;
+}
+
 int main()
 {
+	srand(time(NULL));
 	std::cout << "Functions avaliable in this program: " << std::endl;
 	my_function();
 	std::cout << "I hope you enjoy using the program." << std::endl << std::endl;
@@ -65,6 +80,9 @@ int main()
 			break;
 		case 4:
 			strings();
+			break;
+		case 5:
+			puppies();
 			break;
 		default:
 			break;
